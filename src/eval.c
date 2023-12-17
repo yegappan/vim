e83175111fe372a2c543afb68e576527397fa155
Yegappan Lakshmanan <yegappan@yahoo.com>
2023-12-16 23:20:53 -0800

    Add type alias information to type_T and use that to do proper type checks

--- src/eval.c
+++ src/eval.c
@@ -4738,6 +4738,8 @@ eval8(
 	    clear_type_list(&type_list);
 	    return FAIL;
 	}
+	// When <typealias> cast is used, use the underlying alised type.
+	want_type = RESOLVE_TYPEALIAS(want_type);
 
 	if (**arg != '>')
 	{
